2dba7277323d87898e8f3f4838862dc63b28f3f9
Kalle Leskinen <kalle@leskinen.org>
2020-08-26 13:22:55 +0300

    Assingment1
    wip

---
+++ Assignment1/Assignment1/RealTimeBikes/RealTimeCityBikeDataFetcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Assignment1
+{
+    class RealTimeCityBikeDataFetcher : ICityBikeDataFetcher
+    {
+        public Task<int> GetBikeCountInStation(string stationName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+
+
+    }
+}

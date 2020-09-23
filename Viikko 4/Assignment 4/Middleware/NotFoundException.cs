a409f8c5ecb061b1406be1663c06925480176c26
Kalle Leskinen <kalle@leskinen.org>
2020-09-23 08:44:24 +0300

    2.2

--- Viikko 4/Assignment 4/Middleware/NotFoundException.cs
+++ Viikko 4/Assignment 4/Middleware/NotFoundException.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Assignment_4
-{
-    public class NotFoundException
-    {
-
-        string logError()
-        {
-            return "Player GUID wrong/doesn't exist";
-        }
-
-    }
-}
+﻿
